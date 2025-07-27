cabfa22d1d860cfe7315850f7d5698e9ed068811
abhiramelf <abhiram.elf@gmail.com>
2025-07-27 21:09:59 +0530

    Server initialized

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import 'dotenv/config';   
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("App is running!");
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running at URL\n\nhttp://localhost:${process.env.PORT}`);
+})

e8556c0286357cdf0b2810baea7c52b2ca101244
abhiramelf <abhiram.elf@gmail.com>
2025-08-02 11:50:33 +0530

    Added schema for Users

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please provide a valid email address'
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'], 
+        select: false, // Do not return password by default 
+    }
+},
+{
+        timestamps: true, // Automatically manage createdAt and updatedAt fields
+});
+
+export default mongoose.model("User", userSchema);

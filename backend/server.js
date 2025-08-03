912311aaf4a3fa2d48084ea7ef5bbd1daafe09e3
abhiramelf <abhiram.elf@gmail.com>
2025-08-03 23:39:49 +0530

    Added API documentation endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 // Import required modules
 import express from "express"; // Express framework for building the server
 import 'dotenv/config'; // Load environment variables from .env file
+import swaggerUi from 'swagger-ui-express'; // Swagger UI for API documentation
+import YAML from 'yamljs'; // YAML parser for Swagger configuration
+
 import connectDB from "./config/db.js" // MongoDB connection logic
 import urlRoutes from "./routes/urls.js"; // Import URL routes
 import indexRoutes from "./routes/index.js"; // Import index routes
@@ -24,6 +27,10 @@ app.use("/api", urlRoutes); // Use URL routes under /api path
 app.use("/", indexRoutes); // Use index routes for short URL code handling
 app.use("/api/auth", authRoutes); // Use authentication routes under /api/auth path
 app.use("/api/links", linkRoutes); // Use link routes under /api/links path
+
+// Swagger configuration
+const swaggerDocument = YAML.load('./swagger.yml'); // Load Swagger configuration from YAML file
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Serve Swagger UI at /api-docs path
 
 // Start the server on the specified port
 app.listen(process.env.PORT || 3000, () => {
